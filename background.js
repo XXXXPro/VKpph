51b94e9c684181731bcf82aacb569fd932416930
4X_Pro <me@4xpro.ru>
2020-06-05 17:43:08 +0300

    Fetch request moved to background script Fixed bug: markup_user() wasn't called when pending users list is empty

---
+++ background.js
@@ -0,0 +1,14 @@
+const token = "83821776838217768387f3f5d783d87c348838283821776db4a62ffd123eb33245671c3"; // Service token
+const api_url = "https://api.vk.com/method/users.get?fields=screen_name&name_case=nom&access_token="+token+"&v=5.107&user_ids="; // URL of users.get method
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    const full_api_url = api_url + request.join(","); // building URL to call
+    fetch(full_api_url).then(function(response) { // using fetch to get data
+      if (response.ok && response.status == 200) { // if data received, converting it to JSON
+        response.json().then(sendResponse);
+      }
+    }).catch(param=>sendResponse({}));
+    return true;  // Will respond asynchronously.
+  }
+);
--- vkpph.js
+++ vkpph.js
@@ -3,14 +3,12 @@
 *  URL: https://github.com/XXXXPro/VKpph/
 *  Written by 4X_Pro <me@4xpro.ru>
 *  License: GPL 3.0
-*  ================================*/
+*  ================================ */
 
 (function () { // anonymous self-calling function to avoid name collisions
 let checked = {}; // hash of already being checking or checked users, keys are screen_names values are statuses (0 - checking, 1 - open profile, 2 - private)
 let pending = []; // list of users to check
 const vk_path = "https://vk.com/";
-const token = "83821776838217768387f3f5d783d87c348838283821776db4a62ffd123eb33245671c3"; // Service token
-const api_url = "https://api.vk.com/method/users.get?fields=screen_name&name_case=nom&access_token="+token+"&v=5.107&user_ids="; // URL of users.get method
 
 function fill_users(slct) { // finds all links to users by given CSS selector and puts them to pending list
   let people = document.querySelectorAll(slct);
@@ -25,17 +23,12 @@ function fill_users(slct) { // finds all links to users by given CSS selector an
 
 function make_req_func(users,last) { // simple closure to make functions to call with setTimeout
   return function() {
-    const full_api_url = api_url + users.join(","); // building URL to call
-    fetch(full_api_url).then(function(response) { // using fetch to get data
-      if (response.status == 200) { // if data received, converting it to JSON
-        return response.json();
-      }
-    }).then(function (data) { // and saving user statuses in checked
+    chrome.runtime.sendMessage(users,function (data) {
       for (let i=0; i<data.response.length; i++) {
-         checked[data.response[i].screen_name]=data.response[i].is_closed ? 2 : 1; // 2 for private profiles, 1 for open profile
-      }
-      if (last) mark_users(); // if it is last call, then calling function to add class to private user links
-    }).catch(console.log);
+        checked[data.response[i].screen_name]=data.response[i].is_closed ? 2 : 1; // 2 for private profiles, 1 for open profile
+     }
+     if (last) mark_users(); // if it is last call, then calling function to add class to private user links      
+    })
   }
 }
 function do_request() {
@@ -47,7 +40,7 @@ function do_request() {
     start=start+per_page;
     timer=timer+timer_step;
   }
-  pending=[];
+  if (pending.length==0) mark_users(); // if no users pending, no calls with setTimeout will be done, so we need to call markup function immediately
 }
 function mark_users() {
    for (user in checked) {
@@ -58,6 +51,7 @@ function mark_users() {
         }
      }
    }
+   pending=[];   
 }
 function process(param) {
   if (param==null ||  (param[0].target.className!='im-page--title-status _im_page_status' && param[0].target.className!='im-page--title-meta _im_page_peer_online')) {
@@ -68,6 +62,7 @@ function process(param) {
     fill_users('.reply_image');
     fill_users('.like_tt_owner');
     fill_users('a.friend_recomm_card');
+    fill_users('.fans_fan_ph');
     do_request();
   }
 }
@@ -77,7 +72,6 @@ function clear_cache() {
 
 process(null); // starting for the first time
 let observer = new MutationObserver(process); // and starting to observe, if new elements arrive to restart process function and update highlights
-observer.observe(document.getElementById('page_body'),{attributes: false, childList: true, characterData: false, subtree:true});
+observer.observe(document.body,{attributes: false, childList: true, characterData: false, subtree:true});
 setInterval(clear_cache,2*24*60*60*1000); // clear checked users cache each 2 days for those, who never turns off computer
-
 })();
